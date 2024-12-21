e7560409cdc5c4f276478a8e4ec62bb7ea05b01b
PancakeLord <yoav.zoller@gmail.com>
2024-12-21 20:37:49 +0200

    added rotor start positions for message

--- Enigma.py
+++ Enigma.py
@@ -6,19 +6,29 @@ class Enigma:
     MAX_LETTER = 'Z'
     ALPHABET_LEN = ord(MAX_LETTER) - ord(MIN_LETTER) + 1
     ROTOR_COUNT = 3
+    ROTOR_OFFSET_REPEAT = 2
 
     def __init__(self):
         self.rotor_offsets = [0]*self.ROTOR_COUNT
         self.base_offsets = [0]*self.ROTOR_COUNT
 
-    def encrypt(self, message: str)-> str:
-        return self._eval(message.upper())
+    def encrypt(self, message: str, offsets = [0] * ROTOR_COUNT)-> str:
+        start_message = ''.join([chr((x % Enigma.ALPHABET_LEN)+ord(Enigma.MIN_LETTER)) for x in offsets]) * Enigma.ROTOR_OFFSET_REPEAT
+        start_encryption = self._eval(start_message)
+        self.rotor_offsets = offsets.copy()
+        encryption = self._eval(message.upper())
+        return start_encryption + encryption
 
     def decrypt(self, encryption: str)-> str:
-        return self._eval(encryption.upper())
+        start_encryption = encryption[:Enigma.ROTOR_COUNT*Enigma.ROTOR_OFFSET_REPEAT]
+        actual_encryption = encryption[Enigma.ROTOR_COUNT*Enigma.ROTOR_OFFSET_REPEAT:]
+        start_decrypted = self._eval(start_encryption)
+        self.rotor_offsets = [(ord(start_decrypted[i])-ord(Enigma.MIN_LETTER)) for i in range(Enigma.ROTOR_COUNT)]
+        decrypted = self._eval(actual_encryption)
+        return start_decrypted + decrypted
 
     def _reset(self) -> None:
-        self.rotor_offsets = self.base_offsets
+        self.rotor_offsets = self.base_offsets.copy()
 
     def _zero_curr_offset(self) -> None:
         self.rotor_offsets = [0]*self.ROTOR_COUNT
@@ -70,7 +80,7 @@ class Enigma:
 
 
 enigma = Enigma()
-encryption = enigma.encrypt("We will intercept the american shipment with our U boats at dawn tomorrow!")
+encryption = enigma.encrypt("We will intercept the american shipment with our U boats at dawn tomorrow!", [6,13,19])
 print(encryption)
 decryption = enigma.decrypt(encryption)
 print(decryption)
