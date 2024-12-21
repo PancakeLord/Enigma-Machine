875e8ceb7c7b65379b974e9cce8ae354f8da1209
PancakeLord <yoav.zoller@gmail.com>
2024-12-21 20:59:31 +0200

    Added plugboard

--- Enigma.py
+++ Enigma.py
@@ -8,9 +8,10 @@ class Enigma:
     ROTOR_COUNT = 3
     ROTOR_OFFSET_REPEAT = 2
 
-    def __init__(self):
-        self.rotor_offsets = [0]*self.ROTOR_COUNT
-        self.base_offsets = [0]*self.ROTOR_COUNT
+    def __init__(self, rotor_offsets = [0] * ROTOR_COUNT, base_offsets = [0] * ROTOR_COUNT, plugboard = {}):
+        self.rotor_offsets = rotor_offsets.copy()
+        self.base_offsets = base_offsets.copy()
+        self.plugboard = plugboard.copy()
 
     def encrypt(self, message: str, offsets = [0] * ROTOR_COUNT)-> str:
         start_message = ''.join([chr((x % Enigma.ALPHABET_LEN)+ord(Enigma.MIN_LETTER)) for x in offsets]) * Enigma.ROTOR_OFFSET_REPEAT
@@ -26,6 +27,15 @@ class Enigma:
         self.rotor_offsets = [(ord(start_decrypted[i])-ord(Enigma.MIN_LETTER)) for i in range(Enigma.ROTOR_COUNT)]
         decrypted = self._eval(actual_encryption)
         return start_decrypted + decrypted
+
+    def add_plugboard_connection(self, chr1: chr, chr2: chr) -> None:
+        if chr1 != chr2 and chr1 not in self.plugboard.keys() and  chr2 not in self.plugboard.keys():
+            self.plugboard[chr1] = chr2
+            self.plugboard[chr2] = chr1
+
+    def remove_plugboard_connection(self, chr: chr) -> None:
+        if chr in self.plugboard.keys():
+            self.plugboard.pop(self.plugboard.pop(chr))
 
     def _reset(self) -> None:
         self.rotor_offsets = self.base_offsets.copy()
@@ -48,14 +58,14 @@ class Enigma:
             # In truth, messages on the enigma would not include special characters at all. Sometimes SPACE would be
             # represented by X, so HELLO WORLD -> ENC(HELLOXWORLD)
             return character
-        e = character
+        e = self._plugboard(character)
         for i in range(Enigma.ROTOR_COUNT):
             e = self._rotor(e, i)
         e = self._reflector(e)
         for i in reversed(range(Enigma.ROTOR_COUNT)):
             e = self._rotor(e, i, reverse=True)
         self._tick()
-        return e
+        return self._plugboard(e)
 
     def _tick(self) -> None:
         self._turn_rotor(0)
@@ -69,6 +79,10 @@ class Enigma:
 
     def _reflector(self, character: chr) -> chr:
         return REFLECTOR[ord(character) - ord(Enigma.MIN_LETTER)]
+    def _plugboard(self, character: chr) -> chr:
+        if (not str.isalpha(character)) or character not in self.plugboard.keys():
+            return character
+        return self.plugboard[character]
 
     def _turn_rotor(self, rotor:int) -> None:
         self.rotor_offsets[rotor] += 1
@@ -79,7 +93,7 @@ class Enigma:
                     self._turn_rotor(rotor+1)
 
 
-enigma = Enigma()
+enigma = Enigma(plugboard={"A":"C", "C":"A"})
 encryption = enigma.encrypt("We will intercept the american shipment with our U boats at dawn tomorrow!", [6,13,19])
 print(encryption)
 decryption = enigma.decrypt(encryption)
