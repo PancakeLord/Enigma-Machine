57fb9f822a6cd94453a43c7f6bb8824ff94965cc
PancakeLord <yoav.zoller@gmail.com>
2024-12-21 19:42:57 +0200

    Extra line

--- Enigma.py
+++ Enigma.py
@@ -68,6 +68,7 @@ class Enigma:
                 if rotor < Enigma.ROTOR_COUNT-1:
                     self._turn_rotor(rotor+1)
 
+
 enigma = Enigma()
 encryption = enigma.encrypt("We will intercept the american shipment with our U boats at dawn tomorrow!")
 print(encryption)
