306b392632e1cbbe452d0037f8ea812f76383134
PancakeLord <yoav.zoller@gmail.com>
2024-12-22 16:31:49 +0200

    Added Cyclometer

--- Cyclometer.py
+++ Cyclometer.py
@@ -8,18 +8,32 @@ class Cyclometer:
         self.enigma = Enigma(base_offsets=base_offsets.copy(), rotor_order=rotor_order.copy())
 
     def get_cycles(self) -> list:
-        AD = {chr(x):chr(x) for x in range(ord(Enigma.MIN_LETTER), ord(Enigma.MAX_LETTER)+1)}
-        BE = {chr(x):chr(x) for x in range(ord(Enigma.MIN_LETTER), ord(Enigma.MAX_LETTER)+1)}
-        CF = {chr(x):chr(x) for x in range(ord(Enigma.MIN_LETTER), ord(Enigma.MAX_LETTER)+1)}
+        AD = {}
+        BE = {}
+        CF = {}
         for i in range(Enigma.ALPHABET_LEN):
             message = self.enigma.encrypt("",[i,i,i])
-            AD[message[0]] = AD[message[4]]
-            BE[message[1]]=  BE[message[5]]
-            CF[message[2]] = CF[message[6]]
+            AD[message[0]] = message[3]
+            BE[message[1]] = message[4]
+            CF[message[2]] = message[5]
         cycle_dicts = [AD,BE,CF]
-        cycles = []
+        cycles = [[],[],[]]
         for i in range(3):
             alphabet = [chr(x) for x in range(ord(Enigma.MIN_LETTER), ord(Enigma.MAX_LETTER)+1)]
+            cycle_num = 0
             while len(alphabet) != 0:
-                start = alphabet.pop(0)
+                cycles[i].append([])
+                start = alphabet[0]
+                curr = start
+                ind = 0
+                while curr != start or ind == 0:
+                    alphabet.remove(curr)
+                    cycles[i][cycle_num].append(curr)
+                    curr = cycle_dicts[i][curr]
+                    ind += 1
+                cycle_num += 1
+        return cycles
 
+
+cyclometer = Cyclometer()
+print(cyclometer.get_cycles())
