eb90f988418a70edbaae0263f605ff4b2fa4a93f
PancakeLord <yoav.zoller@gmail.com>
2024-12-23 08:23:12 +0200

    Card Catalog and Cyclometer

--- Cyclometer.py
+++ Cyclometer.py
@@ -1,5 +1,5 @@
 from Enigma import *
-
+from card_catalog import *
 
 class Cyclometer:
     def __init__(self, base_offsets = [0]*Enigma.ROTOR_COUNT, rotor_order = list(range(Enigma.ROTOR_COUNT))):
@@ -43,12 +43,15 @@ def get_string(base_offsets, rotor_order):
 
 permutations = [[0,1,2],[0,2,1],[1,0,2],[2,0,1],[1,2,0],[2,1,0]]
 cyclometer = Cyclometer()
-card_catalog = {}
-for i in range(Enigma.ALPHABET_LEN):
-    for j in range(Enigma.ALPHABET_LEN):
-        for k in range(Enigma.ALPHABET_LEN):
-            for order in permutations:
-                cyclometer.set(base_offsets=[i,j,k],rotor_order=order)
-                card_catalog[get_string(base_offsets=[i,j,k], rotor_order=order)] = cyclometer.get_cycles()
-
-print(card_catalog)
+# card_catalog = {}
+# for i in range(Enigma.ALPHABET_LEN):
+#     for j in range(Enigma.ALPHABET_LEN):
+#         for k in range(Enigma.ALPHABET_LEN):
+#             for order in permutations:
+#                 cyclometer.set(base_offsets=[i,j,k],rotor_order=order)
+#                 card_catalog[get_string(base_offsets=[i,j,k], rotor_order=order)] = cyclometer.get_cycles()
+#
+# with open("card_catalog.py", 'w') as data:
+#     data.write("card_catalog = " + str(card_catalog))
+card_catalog_lengths =  {x: [[len(a) for a in card_catalog[x][i]] for i in range(3)] for x in card_catalog.keys()}
+print(card_catalog_lengths)
